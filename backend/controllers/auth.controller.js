da410fad32781f49f226ff509029fd678d04ca40
sof1516 <teshomesofoniyas152@gmail.com>
2025-05-24 23:21:21 -0400

    sign up and sign in working, started designing the dashboard page

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,12 +2,13 @@ const bcrypt = require("bcrypt");
 // const connectDB = require("../models/db");
 const User = require("../models/user.model");
 
-const signup = async (req, res) => {
+const isValidEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const signUp = async (req, res) => {
   try {
-    const isValidEmail = (email) => {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailRegex.test(email);
-    };
 
     const newUser = req.body;
 
@@ -51,4 +52,49 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = {signup};
+const signIn = async (req, res) => {
+  try {
+    let inComingUser = req.body;
+
+    if (
+      !inComingUser.email ||
+      !inComingUser.password
+    ) {
+      return res.status(400).json({ error: "One or more fields are missing" });
+    }
+
+    if (!isValidEmail(inComingUser.email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    if (inComingUser.password.length < 8) {
+      return res.status(400).json({ error: "Password is too short" });
+    }
+
+    const userFound = await User.findOne({ email: inComingUser.email }).exec();
+
+    if (!userFound) {
+      return res.status(401).json({error: "Invalid username or password"});
+    }
+
+    const isMatch = await bcrypt.compare(inComingUser.password, userFound.password);
+
+    if (!isMatch) {
+      return res.status(401).json({"user": "Invalid username or password"});
+    }
+
+    const userWithOutPassword = {
+      name: userFound.name,
+      username: userFound.username,
+      email: userFound.email
+    };
+    
+    return res.status(200).json({user: userWithOutPassword, "message": "Signed in successfully"});
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal server error" }); 
+  }
+}
+
+module.exports = {signUp, signIn};
