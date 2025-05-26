bb3cf4e191e5488d28ec2182ce2511f83b55f1bf
sof1516 <teshomesofoniyas152@gmail.com>
2025-05-25 23:54:29 -0400

    started making the dashboard page responsive

--- frontend/vendora-frontend/src/components/Dashboard.jsx
+++ frontend/vendora-frontend/src/components/Dashboard.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 
 export default function Dashboard() {
+
+    const [subSection, setSubSection] = useState("myListings");
+
+    const handleSection = (section) => {
+        setSubSection(section);
+    }
+
     return (
         <>
             <Navbar />
             <div className="flex min-h-screen">
                 {/* Left (thinner) block */}
-                <div className="w-1/5 bg-gray-200">
-                    <button className="p-4">My Listings</button>
-                    <button className="p-4">My Listings</button>
-                    <button>My Listings</button>
+                <div className="w-1/5">
+                    <button className="w-full hover:bg-gray-100 cursor-pointer transition p-4 text-left">
+                        <span className="pl-2">My Listings</span></button>
+                    <button className="w-full hover:bg-gray-100 cursor-pointer transition p-4 text-left">
+                        <span className="pl-2">My Bookings</span></button>
+                    <button className="w-full hover:bg-gray-100 cursor-pointer transition p-4 text-left">
+                        <span className="pl-2">Payment Info</span></button>
+                    <button className="w-full hover:bg-gray-100 cursor-pointer transition p-4 text-left">
+                        <span className="pl-2">Reviews & Rating</span></button>
+                    <button className="w-full hover:bg-gray-100 cursor-pointer transition p-4 text-left">
+                        <span className="pl-2">Favorites/Saved</span></button>
+                    <button className="w-full hover:bg-gray-100 cursor-pointer transition p-4 text-left">
+                        <span className="pl-2">Logout</span></button>
                 </div>
 
                 {/* Right (main) block */}
--- frontend/vendora-frontend/src/components/Navbar.jsx
+++ frontend/vendora-frontend/src/components/Navbar.jsx
@@ -5,6 +5,9 @@ export default function Navbar() {
         <>
             <div className="navbar bg-base-100 shadow-sm p-4">
   <div className="flex-1">
+  <button className="btn btn-square btn-ghost">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+    </button>
     <a className="btn btn-ghost text-xl">Vendora</a>
     <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
     <button className="btn">Search</button>
