85f70a4ae331186bfbfb09ba5b4a8fe81e847c2b
sof1516 <teshomesofoniyas152@gmail.com>
2025-06-16 00:04:25 -0400

    serice option and time slot adding ui complete

--- backend/controllers/booking.controller.js
+++ backend/controllers/booking.controller.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
 const createBooking = async (req, res) => {
-    
+    //parse the req body
+    //make sure the necessary attributes are not empty
+    //
 }
--- frontend/vendora-frontend/src/components/Navbar.jsx
+++ frontend/vendora-frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function Navbar() {
     return (
         <>
-            <div className="navbar bg-base-100 shadow-sm p-4">
+            <div className="navbar bg-blue-200 shadow-sm p-4">
   <div className="flex-1">
   <button className="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
