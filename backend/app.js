7977221c3869a5dd2e8d8f67b339aa8432ea5fa1
sof1516 <teshomesofoniyas152@gmail.com>
2025-06-28 23:07:41 -0400

    finished the backend and frontend for fetching mylisting and mybooking, however changes are not showing in these user document arrays

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth.routes.js");
-const listingRoutes = require("./routes/listing.routes.js")
+const listingRoutes = require("./routes/listing.routes.js");
 const bookingRoutes = require("./routes/booking.routes.js");
+const userRoutes = require("./routes/user.routes.js");
 const connectDB = require("./models/db.js");
 
 const app = express();
@@ -15,8 +16,9 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api/auth', authRoutes);
-app.use('/api/listing', listingRoutes)
-app.use('/api/booking', bookingRoutes)
+app.use('/api/listing', listingRoutes);
+app.use('/api/booking', bookingRoutes);
+app.use('/api/user', userRoutes);
 
 app.get('/test', (req, res) => {
     res.json({ message: "Server is working" });
