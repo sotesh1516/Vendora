9e25ff5fe59981fdf0d765f0343a94c16f0857e5
sof1516 <teshomesofoniyas152@gmail.com>
2025-07-09 23:50:11 -0400

    started my listing fetch feature

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { updateUserBookingList, updateUserListingList, fetchUserBookingList } = require("../controllers/user.controller");
+const { updateUserBookingList, updateUserListingList, fetchUserBookingList, fetchUserListingList } = require("../controllers/user.controller");
 const router = express.Router();
 
 router.post("/mybooking/update", updateUserBookingList);
 router.post("/mylisting/update", updateUserListingList);
 router.post("/mybookings/fetch", fetchUserBookingList);
+router.post("/mylistings/fetch", fetchUserListingList);
 
 module.exports = router;
