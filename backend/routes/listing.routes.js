73bdad1c326d725cc5c2e81f3119cbb0fd1aa11c
sof1516 <teshomesofoniyas152@gmail.com>
2025-07-17 23:55:29 -0400

    started implementing search feature

--- backend/routes/listing.routes.js
+++ backend/routes/listing.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const { createListing, fetchListings, fetchListingsAndSetFavorites } = require("../controllers/listing.controller");
+const { createListing, fetchListings, fetchListingsAndSetFavorites, searchListings } = require("../controllers/listing.controller");
 const router = express.Router();
 
 
 router.post("/create", createListing);
 // router.get("/fetch", fetchListings); this does filter the user favorites
 router.post("/fetch", fetchListingsAndSetFavorites);
+//this needs to be changed, the use of verbs inside an end point is not a good practice
+router.get("/search", searchListings)
 
 module.exports = router;
